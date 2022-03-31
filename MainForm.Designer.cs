d5ec739d57b95ffeac03444b5929613333301077
田中信秋 <sinshu.d@gmail.com>
2022-04-01 00:06:34 +0900

    Add files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace NoiseGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblFrequency = new System.Windows.Forms.Label();
+            this.barVolume = new System.Windows.Forms.TrackBar();
+            this.barFrequency = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.barVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barFrequency)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(8, 8);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(43, 12);
+            this.lblVolume.TabIndex = 0;
+            this.lblVolume.Text = "Volume";
+            // 
+            // lblFrequency
+            // 
+            this.lblFrequency.AutoSize = true;
+            this.lblFrequency.Location = new System.Drawing.Point(8, 64);
+            this.lblFrequency.Name = "lblFrequency";
+            this.lblFrequency.Size = new System.Drawing.Size(58, 12);
+            this.lblFrequency.TabIndex = 1;
+            this.lblFrequency.Text = "Frequency";
+            // 
+            // barVolume
+            // 
+            this.barVolume.Location = new System.Drawing.Point(0, 24);
+            this.barVolume.Maximum = 100;
+            this.barVolume.Name = "barVolume";
+            this.barVolume.Size = new System.Drawing.Size(800, 45);
+            this.barVolume.TabIndex = 2;
+            this.barVolume.Value = 20;
+            this.barVolume.Scroll += new System.EventHandler(this.barVolume_Scroll);
+            // 
+            // barFrequency
+            // 
+            this.barFrequency.Location = new System.Drawing.Point(0, 80);
+            this.barFrequency.Maximum = 100;
+            this.barFrequency.Name = "barFrequency";
+            this.barFrequency.Size = new System.Drawing.Size(800, 45);
+            this.barFrequency.TabIndex = 3;
+            this.barFrequency.Value = 20;
+            this.barFrequency.Scroll += new System.EventHandler(this.barFrequency_Scroll);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 120);
+            this.Controls.Add(this.barFrequency);
+            this.Controls.Add(this.lblFrequency);
+            this.Controls.Add(this.barVolume);
+            this.Controls.Add(this.lblVolume);
+            this.Name = "MainForm";
+            this.Text = "NoiseGenerator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.barVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barFrequency)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblFrequency;
+        private System.Windows.Forms.TrackBar barVolume;
+        private System.Windows.Forms.TrackBar barFrequency;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace NoiseGenerator
+{
+    public partial class MainForm : Form
+    {
+        private WaveOut waveOut;
+        private NoiseSampleProvider noiseSampleProvider;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            if (waveOut == null)
+            {
+                noiseSampleProvider = new NoiseSampleProvider();
+                waveOut = new WaveOut(WaveCallbackInfo.ExistingWindow(Handle));
+                waveOut.Init(noiseSampleProvider);
+                waveOut.Play();
+            }
+
+            SetGain((double)barVolume.Value / barVolume.Maximum);
+            SetFrequency((double)barFrequency.Value / barFrequency.Maximum);
+        }
+
+        private void SetGain(double gain)
+        {
+            lblVolume.Text = "Volume (" + gain.ToString("0.00") + ")";
+            noiseSampleProvider.SetGain(gain);
+        }
+
+        private void SetFrequency(double frequency)
+        {
+            lblFrequency.Text = "Frequency (" + frequency.ToString("0.00") + ")";
+            noiseSampleProvider.SetFrequency(frequency);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (waveOut != null)
+            {
+                waveOut.Dispose();
+                waveOut = null;
+            }
+        }
+
+        private void barVolume_Scroll(object sender, EventArgs e)
+        {
+            SetGain((double)barVolume.Value / barVolume.Maximum);
+        }
+
+        private void barFrequency_Scroll(object sender, EventArgs e)
+        {
+            SetFrequency((double)barFrequency.Value / barFrequency.Maximum);
+        }
+
+
+
+        private class NoiseSampleProvider : ISampleProvider
+        {
+            private static WaveFormat format = WaveFormat.CreateIeeeFloatWaveFormat(48000, 1);
+
+            private Random random;
+            private double gain;
+            private BiQuadFilter filter;
+
+            public NoiseSampleProvider()
+            {
+                random = new Random();
+                gain = 0;
+                filter = new BiQuadFilter(format.SampleRate);
+            }
+
+            public void SetGain(double gain)
+            {
+                this.gain = 10 * gain;
+            }
+
+            public void SetFrequency(double frequency)
+            {
+                filter.SetLowPassFilter((float)(0.1 * frequency * format.SampleRate), 1F);
+            }
+
+            public int Read(float[] buffer, int offset, int count)
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    buffer[offset + i] = (float)(gain * (random.NextDouble() - 0.5));
+                }
+
+                filter.Process(buffer, offset, count);
+
+                return count;
+            }
+
+            public WaveFormat WaveFormat => format;
+        }
+
+
+
+        private class BiQuadFilter
+        {
+            private static readonly float resonancePeakOffset = (float)(1 - 1 / Math.Sqrt(2));
+
+            private int sampleRate;
+
+            private float a0;
+            private float a1;
+            private float a2;
+            private float a3;
+            private float a4;
+
+            private float x1;
+            private float x2;
+            private float y1;
+            private float y2;
+
+            public BiQuadFilter(int sampleRate)
+            {
+                this.sampleRate = sampleRate;
+            }
+
+            public void ClearBuffer()
+            {
+                x1 = 0;
+                x2 = 0;
+                y1 = 0;
+                y2 = 0;
+            }
+
+            public void SetLowPassFilter(float cutoffFrequency, float resonance)
+            {
+                // This equation gives the Q value which makes the desired resonance peak.
+                // The error of the resultant peak height is less than 3%.
+                var q = resonance - resonancePeakOffset / (1 + 6 * (resonance - 1));
+
+                var w = 2 * Math.PI * cutoffFrequency / sampleRate;
+                var cosw = Math.Cos(w);
+                var alpha = Math.Sin(w) / (2 * q);
+
+                var b0 = (1 - cosw) / 2;
+                var b1 = 1 - cosw;
+                var b2 = (1 - cosw) / 2;
+                var a0 = 1 + alpha;
+                var a1 = -2 * cosw;
+                var a2 = 1 - alpha;
+
+                SetCoefficients((float)a0, (float)a1, (float)a2, (float)b0, (float)b1, (float)b2);
+            }
+
+            public void Process(float[] buffer, int offset, int count)
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    var t = offset + i;
+
+                    var input = buffer[t];
+                    var output = a0 * input + a1 * x1 + a2 * x2 - a3 * y1 - a4 * y2;
+
+                    x2 = x1;
+                    x1 = input;
+                    y2 = y1;
+                    y1 = output;
+
+                    buffer[t] = output;
+                }
+            }
+
+            private void SetCoefficients(float a0, float a1, float a2, float b0, float b1, float b2)
+            {
+                this.a0 = b0 / a0;
+                this.a1 = b1 / a0;
+                this.a2 = b2 / a0;
+                this.a3 = a1 / a0;
+                this.a4 = a2 / a0;
+            }
+        }
+    }
+}
